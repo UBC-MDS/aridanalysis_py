6e182c11fb19972c33a044e1fbe99aa705f91d1a
cmmclaug <cmmclaug@gmail.com>
2021-02-26 21:42:39 -0800

    linreg updates

--- aridanalysis/aridanalysis.py
+++ aridanalysis/aridanalysis.py
@@ -1,3 +1,33 @@
+def arid_linreg(df, response, features = [], estimator = None, regularization = None):
+    """
+    Function that performs a linear regression on continuous response data. 
+    This function will fit a linear regression model on the input .
+
+    Parameters
+    ----------
+    df : pandas.Dataframe
+        A dataframe that contains the data to be analized
+    response : string
+        A column name of the response variable
+    features : list (optional)
+        A list of the chosen explanatory feature columns
+    estimator : function (optional)
+        The function used to fit the linear regression model The default is OLS
+    regularization : string (optional)
+        What level of regularization to use in the model values:
+        * L1 * L2 * L1L2 *
+        
+    Returns
+    -------
+    pandas.DataFrame
+        A pandas dataframe with a list of features, and their coefficients 
+
+    Examples
+    --------
+    >>> from aridanalysis import aridanalysis
+    >>> aridanalysis.arid_linreg(df, income)
+    """
+    return None
 
 def arid_countreg():
     """
